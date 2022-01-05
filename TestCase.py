1ac5b6b081e33fb16730fb99e649cdb426a1e16a
Ganesh Mane <96051353+ganeshsmane@users.noreply.github.com>
2022-01-05 15:07:31 +0530

    TestCase.py

---
+++ TestCase.py
@@ -0,0 +1,27 @@
+#input list=[5,3,8,2]
+#output=222
+
+
+def multiplication(value):
+    mult=1
+    for i in range(len(value)):
+        mult=mult*value[i]
+    return mult
+
+def addition(value1):
+    add=0
+    for i in range(len(value1)):
+        add=add+value1[i]
+    return add
+def main():
+    data=[5,3,8,2]
+    
+    mult=multiplication(data)
+    
+    add=addition(data)
+    
+    ret=mult-add
+    print("Ans is: ",ret)
+    
+if __name__=="__main__":
+  main()
